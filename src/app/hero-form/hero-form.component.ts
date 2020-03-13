252a4446141edbacdebf21da56974f7272d1e53d
ShayRomayo <noahking10@gmail.com>
2020-03-13 10:56:31 -0500

    Implement clear option at parent level for hero-form

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -12,13 +12,12 @@ export class HeroFormComponent implements OnInit {
   orgs = Teams;
   orgOptions = [];
   @Input() hero: Hero;
+  @Input() clear: boolean;
   @Output() onHeroSubmit: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   //TODO: Beautify with the CSS sheets. Possible Bootstrap for Reactive Web Design
-  //TODO: Move clear functionality to the parent component, so it's an optional clear
-  //TODO: Move fav checkbox into the hero-form
   public submit(name: string, org: string, fav: boolean) {
     let data = [name, org, fav];
     console.log(`Log:${this.hero}`);
